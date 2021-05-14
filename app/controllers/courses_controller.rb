79478a8df5da95b77de2c9a1bc880eb628f589a6
ligiadol <ligia@mdoliveira.com>
2021-05-14 19:16:07 -0300

    Cadastrar um curso

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,7 +3,21 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def new 
+    @course = Course.new
+  end
+
   def show
     @course = Course.find(params[:id])
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.save
+    redirect_to @course
+  end
+
+  def course_params
+    params.require(:course).permit(:name,:description,:code,:price,:enrollment_deadline)
   end
 end
