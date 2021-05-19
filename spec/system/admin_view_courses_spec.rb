96def233903f0614ca4529834246bf8476e0dd78
ligiadol <ligia@mdoliveira.com>
2021-05-19 17:12:54 -0300

    WIP Curso tem um professor

--- spec/system/admin_view_courses_spec.rb
+++ spec/system/admin_view_courses_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 describe 'Admin view courses' do
   it 'successfully' do
+    jorge = Docent.create(name: 'Jorge', email: 'jorge@docent.com', 
+                          bio: 'Um professor chamado Jorge')                
+    jorge.profile_picture.attach(io: File.open('./spec/files/jorge-dazai.jpg'),filename: 'jorge-dazai.jpg') 
     Course.create!(name: 'Ruby', description: 'Um curso de Ruby',
                    code: 'RUBYBASIC', price: 10,
-                   enrollment_deadline: '22/12/2033')
+                   enrollment_deadline: '22/12/2033', instructor: jorge)
     Course.create!(name: 'Ruby on Rails',
                    description: 'Um curso de Ruby on Rails',
                    code: 'RUBYONRAILS', price: 20,
-                   enrollment_deadline: '20/12/2033')
+                   enrollment_deadline: '20/12/2033', instructor: jorge)
 
     visit root_path
     click_on 'Cursos'
@@ -22,13 +25,16 @@ describe 'Admin view courses' do
   end
 
   it 'and view details' do
+    jorge = Docent.create(name: 'Jorge', email: 'jorge@docent.com', 
+                          bio: 'Um professor chamado Jorge')                
+    jorge.profile_picture.attach(io: File.open('./spec/files/jorge-dazai.jpg'),filename: 'jorge-dazai.jpg') 
     Course.create!(name: 'Ruby', description: 'Um curso de Ruby',
                    code: 'RUBYBASIC', price: 10,
-                   enrollment_deadline: '22/12/2033')
+                   enrollment_deadline: '22/12/2033', instructor: jorge)
     Course.create!(name: 'Ruby on Rails',
                    description: 'Um curso de Ruby on Rails',
                    code: 'RUBYONRAILS', price: 20,
-                   enrollment_deadline: '20/12/2033')
+                   enrollment_deadline: '20/12/2033', instructor: jorge)
 
     visit root_path
     click_on 'Cursos'
@@ -37,6 +43,7 @@ describe 'Admin view courses' do
     expect(page).to have_content('Ruby on Rails')
     expect(page).to have_content('Um curso de Ruby on Rails')
     expect(page).to have_content('RUBYONRAILS')
+    expect(page).to have_content('Jorge')
     expect(page).to have_content('R$ 20,00')
     expect(page).to have_content('20/12/2033')
   end
@@ -49,9 +56,12 @@ describe 'Admin view courses' do
   end
 
   it 'and return to home page' do
+    jorge = Docent.create(name: 'Jorge', email: 'jorge@docent.com', 
+                          bio: 'Um professor chamado Jorge')                
+    jorge.profile_picture.attach(io: File.open('./spec/files/jorge-dazai.jpg'),filename: 'jorge-dazai.jpg') 
     Course.create!(name: 'Ruby', description: 'Um curso de Ruby',
                    code: 'RUBYBASIC', price: 10,
-                   enrollment_deadline: '22/12/2033')
+                   enrollment_deadline: '22/12/2033', instructor: jorge)
 
     visit root_path
     click_on 'Cursos'
@@ -61,9 +71,12 @@ describe 'Admin view courses' do
   end
 
   it 'and return to promotions page' do
+    jorge = Docent.create(name: 'Jorge', email: 'jorge@docent.com', 
+                          bio: 'Um professor chamado Jorge')                
+    jorge.profile_picture.attach(io: File.open('./spec/files/jorge-dazai.jpg'),filename: 'jorge-dazai.jpg') 
     Course.create!(name: 'Ruby', description: 'Um curso de Ruby',
                    code: 'RUBYBASIC', price: 10,
-                   enrollment_deadline: '22/12/2033')
+                   enrollment_deadline: '22/12/2033', instructor: jorge)
 
     visit root_path
     click_on 'Cursos'
